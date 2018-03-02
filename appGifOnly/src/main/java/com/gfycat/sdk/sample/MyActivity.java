c39664ee2914ee8be60a09ccc12ae85930195128
Stanislav <dekalo.stanislav@gmail.com>
2018-03-02 14:36:37 +0200

    Add appGifOnly example and raise SDK version to 0.5.0

---
+++ appGifOnly/src/main/java/com/gfycat/sdk/sample/MyActivity.java
@@ -0,0 +1,21 @@
+package com.gfycat.sdk.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.gfycat.core.FeedIdentifier;
+import com.gfycat.core.gfycatapi.pojo.Gfycat;
+
+public class MyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gfycat_sdk_sample);
+    }
+
+    public boolean gfycatSelected(FeedIdentifier identifier, Gfycat gfycat, int position) {
+        startActivity(GfycatViewActivity.createIntent(this, gfycat));
+        return false;
+    }
+}
